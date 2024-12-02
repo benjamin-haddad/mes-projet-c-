36d86ed719492c7a4bbf74ecbacc0b2207e27287
benjamin-haddad <159131593+benjamin-haddad@users.noreply.github.com>
2024-12-02 10:25:19 +0100

    Add files via upload

---
+++ code.c
@@ -0,0 +1,29 @@
+#include <stdio.h>
+
+int main(void)
+{
+  
+     //declaration et affectation d'une variable de type entier (int)
+      const float PI =3.14;  // aireDuCarre =0xF500DA
+    
+    printf ("PI = %.2f\n", PI);
+   
+        PI = 5,14;
+
+        printf ("PI = %.2f\n", PI);
+   /*  
+       %d : nombre entier (int)
+       %f : nombre flottant (float)
+       %c : Caractère (char)
+       %s : chaîne de caractères (texte)
+   */
+
+
+
+    
+    
+    
+    return 0;
+    
+}
+
---
+++ liste a connaitre.c
@@ -0,0 +1,35 @@
+
+
+// LES PRINCIPAUX INFORMATION A CONNAITRE EN LANGUAGE C !!!
+
+
+/* lorsque on débute un programme en c peut importe toujours faire ceci
+
+#include <stdio.h>
+{
+int main (void)
+
+
+return 0;
+}
+*/
+
+// cest la règle d'or n° 1
+
+
+/* const =constante
+   int= un entier
+   printf = afficher
+   main = gère lexecution du programme
+   float =
+   char =
+   void =
+   
+
+
+
+
+
+*/
+
+ 
