08b0e5d20c8ea29a5008aea267f08a08ce3b2bfc
benjamin-haddad <159131593+benjamin-haddad@users.noreply.github.com>
2024-12-06 12:09:11 +0100

    Add files via upload

--- liste a connaitre.c
+++ liste a connaitre.c
@@ -24,12 +24,39 @@ return 0;
    float =
    char =
    void =
+   register =  se situe ou execute le programme dans le registre
+   volatile =  ne va jamais s'executer dans le registre
+
+
+#include <stdio.h>
+
+int main(void)
+{
+  
+     //declaration et affectation d'une variable de type entier (int)
+      const float PI =3.14;  // aireDuCarre =0xF500DA
+    
+    printf ("PI = %.2f\n", PI);
    
+        PI = 5,14;
+
+        printf ("PI = %.2f\n", PI);
+   /*  
+       %d : nombre entier (int)
+       %f : nombre flottant (float)
+       %c : Caractère (char)
+       %s : chaîne de caractères (texte)
 
 
+       
+   */
 
-
-
-*/
-
- 
+   /*
+   
+   maVariable : contenue de la variable(exemple maVariable = 15)
+   &maVariable : adresse ou est stocké la variable
+   
+   
+   
+   
+   */
